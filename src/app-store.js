f72b3b18a481eac05531fc2759a84cd27825ecbc
Ruby Boyarski <rubyboy@gmail.com>
2015-12-09 15:45:46 +0200

    Added createDispatcher to the AppStore

--- src/app-store.js
+++ src/app-store.js
@@ -6,12 +6,21 @@ var AppStore = (function () {
         this.getState = function () {
             return store.getState();
         };
-        this.subscribe = function (subscribeFunction) {
-            // decorate the subscription with the state passed in as a parameter
-            return store.subscribe(function () { return subscribeFunction(store.getState()); });
+        this.subscribe = function (subscriber) {
+            // decorate the subscriber with the state passed in as a parameter
+            return store.subscribe(function () { return subscriber(store.getState()); });
         };
         this.dispatch = function (action) {
             return store.dispatch(action);
+        };
+        this.createDispatcher = function (actionCreator) {
+            return function () {
+                var args = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    args[_i - 0] = arguments[_i];
+                }
+                return store.dispatch(actionCreator.apply(void 0, args));
+            };
         };
     }
     return AppStore;
