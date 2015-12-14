fda0f5237a9eca0dc5c36fb252c03f90c282245c
Ruby Boyarski <rubyboy@gmail.com>
2015-12-14 13:25:24 +0200

    chore(release): added tests

---
+++ test/sanity-tests.js
@@ -0,0 +1,8 @@
+describe('axioms', function() {
+    it('Math works', function() {
+        expect(1+2).toBe(3);
+    });
+    xit("Should be skipped", function() {
+        expect("this").toEqual("not this");
+    });
+});
