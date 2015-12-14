f237410c9c81451c2cae121eec8d2127201897f3
Ruby Boyarski <rubyboy@gmail.com>
2015-12-14 13:48:07 +0200

    chore(test): test

--- src/actions.ts
+++ src/actions.ts
@@ -6,6 +6,7 @@ import {AppStore} from "./app-store"
 export class Actions {
 
     createDispatcher(appStore:AppStore, action:(...n:any[])=>any):()=>void {
+        console.log("here");
         return (...n)=>appStore.dispatch(action.call(this, ...n))
     }
 
