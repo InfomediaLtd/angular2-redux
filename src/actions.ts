f469935e434346c25ab39d20e5021eac02a47735
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-02-17 08:34:17 +0200

    Fixed lint issue

--- src/actions.ts
+++ src/actions.ts
@@ -8,11 +8,15 @@ export class Actions {
     private appStore:AppStore = null;
 
     constructor(appStore?:AppStore) {
-        if (appStore) this.appStore = appStore;
+        if (appStore) {
+          this.appStore = appStore;
+        }
     }
 
     public createDispatcher(action:(...n:any[])=>any, appStore?:AppStore):(...n)=>void {
-        if (!appStore && !this.appStore) throw new Error("Can't find AppStore for dispatching action");
+        if (!appStore && !this.appStore) {
+          throw new Error("Can't find AppStore for dispatching action");
+        }
         appStore = appStore || this.appStore;
         return (...n)=>appStore.dispatch(action.call(this, ...n))
     }
