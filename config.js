1a2c4ca1ab692f17ec6693a96c27fc5bda134b69
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-04-02 20:18:12 +1100

    feat: Update redux-thunk dependency to 2.0.1
    https://github.com/InfomediaLtd/angular2-redux/issues/6

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ System.config({
     "crypto": "github:jspm/nodelibs-crypto@0.1.0",
     "css": "github:systemjs/plugin-css@0.1.20",
     "redux": "npm:redux@3.3.1",
-    "redux-thunk": "npm:redux-thunk@1.0.3",
+    "redux-thunk": "npm:redux-thunk@2.0.1",
     "reflect-metadata": "npm:reflect-metadata@0.1.3",
     "typescript": "npm:typescript@1.8.9",
     "zone.js": "npm:zone.js@0.6.4",
